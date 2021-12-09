1d6aa62d9a3fe00f7e28f8871f92c489f4c560ab
Manoj <manojvivek143@GMAIL.COM>
2021-12-09 12:25:33 +0530

    added

---
+++ servletsdemos/src/main/java/com/tech/servlets/MyFirstServlet.java
@@ -0,0 +1,54 @@
+package com.tech.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		String name = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+		if(name.equals("manoj") && pwd.equals("1234")) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WelcomePageDem");
+			dispatcher.forward(request, response);
+		}else {
+			writer.println("Inavlid user name and password");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+			dispatcher.include(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
